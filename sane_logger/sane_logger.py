f5b3c9b57a61f822f728848ab899837a6c6bfd70
Jesse Aldridge <jessealdridge@gmail.com>
2021-03-12 13:49:56 -0600

    timezone

--- sane_logger/sane_logger.py
+++ sane_logger/sane_logger.py
@@ -8,7 +8,7 @@ def sane_logger(log_level=logging.INFO):
   sh = logging.StreamHandler(sys.stdout)
   formatter = logging.Formatter(
     '[%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s',
-    datefmt='%a, %d %b %Y %H:%M:%S'
+    datefmt='%a, %d %b %Y %H:%M:%S %Z'
   )
   sh.setFormatter(formatter)
   logger.addHandler(sh)
